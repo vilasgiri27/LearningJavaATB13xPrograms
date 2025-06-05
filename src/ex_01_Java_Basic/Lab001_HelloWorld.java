83a290bd8b347da3be274a0f64522fb43584d6d0
vilas giri <vilasgiri1001@gmail.com>
2025-06-05 07:59:43 +0530

    Initial commit

---
+++ src/ex_01_Java_Basic/Lab001_HelloWorld.java
@@ -0,0 +1,8 @@
+package ex_01_Java_Basic;
+
+public class Lab001_HelloWorld {
+    public static void main(String args[])
+    {
+        System.out.println("hello vilas this is first program..");
+    }
+}
