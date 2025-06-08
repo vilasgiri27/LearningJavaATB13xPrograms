eda836e256ac9013470fb4f2b11cc3ab27fcdbc9
vilas giri <vilasgiri1001@gmail.com>
2025-06-08 22:19:16 +0530

    fourth program

---
+++ src/ex_01_Java_Basic/Lab004.java
@@ -0,0 +1,16 @@
+package ex_01_Java_Basic;
+
+public class Lab004 {
+    public static void main(String args[])
+    {
+        System.out.println("sundayprograming");
+
+        int a=10;
+        int b=20;
+        int c=0;
+
+        c=a+b;
+        System.out.println("sum is c:"+c);
+
+    }
+}
