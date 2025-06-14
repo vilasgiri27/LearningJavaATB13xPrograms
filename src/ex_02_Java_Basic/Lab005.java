3ccefd0cf214147ff8aec184d4fc6750e6090b4c
vilas giri <vilasgiri1001@gmail.com>
2025-06-14 17:56:01 +0530

    14/05/2025 program added

---
+++ src/ex_02_Java_Basic/Lab005.java
@@ -0,0 +1,14 @@
+package ex_02_Java_Basic;
+
+public class Lab005 {
+
+    public static void main(String[] args) {
+        {
+            System.out.println("hello world-1");
+            System.out.println("hello world-2");
+            System.out.println("hello world-3");
+            System.out.println("hello world-4");
+            System.out.println("hello 5");
+        }
+    }
+}
